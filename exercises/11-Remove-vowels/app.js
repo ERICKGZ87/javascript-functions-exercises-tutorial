265a82242e22a2b59b70e6e2dec6bdea2ff92b44
ERICKGZ87 <erickgz87@gmail.com>
2021-10-04 23:04:14 +0000

    mis funciones

--- exercises/11-Remove-vowels/app.js
+++ exercises/11-Remove-vowels/app.js
@@ -5,9 +5,10 @@ const rapid = (myString) => {
         if(['a','e','i','o','u'].includes(myString[position]) == false)
             consonants.push(myString[position])
     }
-    return consonants.join('');
+    return consonants.join('').toUpperCase();
 }
 
 // From this line up Do not change code below
 let str = "John";
+
 console.log(rapid(str));
